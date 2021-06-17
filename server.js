a7ff8d009109723a3afc942e77d5b880e2efdb97
reiyon <nsprae@gmail.com>
2021-06-17 16:44:15 +0200

    Connect to database

--- server.js
+++ server.js
@@ -7,5 +7,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const database = mysql.createConnection({
+  host: 'localhost',
+  user: 'prae',
+  password: 'mykomul1',
+  database: 'thinking_routines',
+});
+
+// app.get('/api/insert', (request, response) => {
+//   const sqlInsert = "INSERT INTO used_to_think (content) VALUES ('Cats hate water.');";
+//   database.query(sqlInsert, (error, result) => {
+//     if (error) throw error;
+//     console.log(result);
+//     response.send('inserted');
+//   });
+// });
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
